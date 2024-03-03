2436847b7627a3078fe8331c792227cdaf8c3a95
nikhil <kumarnikil220@gmail.com>
2024-03-04 02:26:23 +0530

    generate a simple dynamic page

--- src/main/java/com/nik/FirstApplication.java
+++ src/main/java/com/nik/FirstApplication.java
@@ -1,15 +1,17 @@
 package com.nik;
-
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class FirstApplication extends HttpServlet{
 	
-	public void service(HttpServletRequest req,HttpServletResponse res ){
+	public void service(HttpServletRequest req,HttpServletResponse res )throws IOException{
 		String str1=req.getParameter("first name");
 		String str2=req.getParameter("last name");
-		String str3="Hello"+str1+" welcome to servlets";
-		System.out.print(str3);
+		String str3="Hello "+ str1 +" welcome to servlets";
+		PrintWriter out=res.getWriter();
+		out.println(str3);
 	}
 }
